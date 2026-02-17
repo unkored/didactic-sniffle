8f1fe23b0a6e4ccdc32d7e9f91c7ca1746027164
unkored <yahwehfchen@gmail.com>
2026-02-17 14:13:12 +0530

    feat: add uc2-OOPSBanner

---
+++ main2.java
@@ -0,0 +1,15 @@
+public class main2 {
+
+  public static void main(String[] args) {
+    System.out.println("   ***   "+" "+"   ***   "+" "+"******   "+" "+"   ***** ");
+    System.out.println(" **   ** "+" "+" **   ** "+" "+"**    ** "+" "+" **      ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**     **"+" "+"**       ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**    ** "+" "+"  **     ");
+    System.out.println("**     **"+" "+"**     **"+" "+"******   "+" "+"    ***  ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
+    System.out.println("**     **"+" "+"**     **"+" "+"**       "+" "+"      ** ");
+    System.out.println(" **   ** "+" "+" **   ** "+" "+"**       "+" "+"      ** ");
+    System.out.println("   ***   "+" "+"   ***   "+" "+"**       "+" "+" *****  ");
+
+  }
+}
